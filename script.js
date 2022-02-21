7f4a94ad3f4a67e78cc4ab3b7f725c0d8a9cd55f
RomanMitaki <96333322+RomanMitaki@users.noreply.github.com>
2022-02-21 18:52:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -136,8 +136,8 @@ const profileAddButton = document.querySelector(
 
 function appearanceCreateCardFormPopup() {
   popup.nextSibling.classList.toggle("popup_opened");
-  addFormInputName.value = '';
-  addFormInputDescription.value = '';
+  addFormInputName.value = "";
+  addFormInputDescription.value = "";
 }
 
 profileAddButton.addEventListener("click", appearanceCreateCardFormPopup);
@@ -153,7 +153,7 @@ popupCreateCardCloseButton.addEventListener(
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//--------------------------------Делаем функционал по добавлению карточки пользователем----------------------------------------------------------------------------
+//--------------------------------Реализуем функционал по добавлению карточки пользователем-------------------------------------------------------------------------
 
 const popupAddForm = popup.nextSibling.querySelector(
   "div.popup__container .popup__edit-form"
@@ -168,11 +168,57 @@ const addFormInputDescription = addFormPopupInputs[1];
 
 function addFormSubmitHandler(evt) {
   evt.preventDefault();
+  //Создаем карточку из темплейта
   const card = cardTemplate.querySelector(".card").cloneNode(true);
   card.querySelector(".card__picture").src = addFormInputDescription.value;
   card.querySelector(".card__picture").alt = addFormInputName.value;
   card.querySelector("div.card__caption .card__text").textContent =
     addFormInputName.value;
+  //Реализуем функционал лайка
+  card
+    .querySelector("div.card__caption .card__like-icon")
+    .addEventListener("click", function (evt) {
+      evt.target.classList.toggle("card__like-icon_active");
+    });
+  //Реализуем функционал удаления карточки
+  card
+    .querySelector(".card__trash-icon")
+    .addEventListener("click", function () {
+      card.querySelector(".card__trash-icon").closest(".card").remove();
+    });
+  //Реализуем функционал зума картинки (создание попапа с картинкой из темплейта с заданными значениями + возможность закрытия попапа через удаление)
+  card
+    .querySelector(".card__pic-button")
+    .addEventListener("click", function () {
+      const picPopupTemplate = document.querySelector("#picPopup").content;
+      const addPicPopup = picPopupTemplate
+        .querySelector(".picPopup")
+        .cloneNode(true);
+      const cardImage = card.querySelector(
+        "button.card__pic-button .card__picture"
+      ).src;
+      const cardCaption = card.querySelector(
+        "div.card__caption .card__text"
+      ).textContent;
+      addPicPopup.querySelector(
+        "div.picPopup__container figure.picPopup__image-container .picPopup__picture"
+      ).src = cardImage;
+      addPicPopup.querySelector(
+        "div.picPopup__container figure.picPopup__image-container .picPopup__picture"
+      ).alt = cardCaption;
+
+      addPicPopup.querySelector(
+        "div.picPopup__container figure.picPopup__image-container .picPopup__caption"
+      ).textContent = cardCaption;
+      const picPopupCloseButton = addPicPopup.querySelector(
+        "div.picPopup__container .picPopup__close-button"
+      );
+      picPopupCloseButton.addEventListener("click", function () {
+        picPopupCloseButton.closest(".picPopup").remove();
+      });
+      document.body.prepend(addPicPopup);
+    });
+
   if (addFormInputName.value === "") {
     card.querySelector("div.card__caption .card__text").textContent =
       "Описания нет";
@@ -187,5 +233,59 @@ function addFormSubmitHandler(evt) {
 }
 
 popupAddForm.addEventListener("submit", addFormSubmitHandler);
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------Реализуем функционал лайка карточки, ее удаления и зума (все кроме добавляемых пользователем)-----------------------------------
+
+//Объявляем переменную для работы с коллекцией изначально существующих карточек
+//и проходимся по ней через for
+const cardArr = cardContainer.querySelectorAll(".card");
+for (let i = 0; i < cardArr.length; i++) {
+  //Добавляем функционал для лайка всей коллекции
+  const likeButton = cardArr[i].querySelector(
+    "div.card__caption .card__like-icon"
+  );
+  //Добавляем функционал удаления карточки коллекции
+  const trashButton = cardArr[i].querySelector(".card__trash-icon");
+  likeButton.addEventListener("click", function (evt) {
+    evt.target.classList.toggle("card__like-icon_active");
+  });
+  trashButton.addEventListener("click", function () {
+    trashButton.closest(".card").remove();
+  });
+  //Добавляем функционал зума картинки(создание попапа из темплейта) и ее закрытия(удаления попапа) для всей коллекции
+  function picPopup() {
+    const picPopupTemplate = document.querySelector("#picPopup").content;
+    const addPicPopup = picPopupTemplate
+      .querySelector(".picPopup")
+      .cloneNode(true);
+    const cardImage = cardArr[i].querySelector(
+      "button.card__pic-button .card__picture"
+    ).src;
+    const cardCaption = cardArr[i].querySelector(
+      "div.card__caption .card__text"
+    ).textContent;
+    addPicPopup.querySelector(
+      "div.picPopup__container figure.picPopup__image-container .picPopup__picture"
+    ).src = cardImage;
+    addPicPopup.querySelector(
+      "div.picPopup__container figure.picPopup__image-container .picPopup__picture"
+    ).alt = cardCaption;
+
+    addPicPopup.querySelector(
+      "div.picPopup__container figure.picPopup__image-container .picPopup__caption"
+    ).textContent = cardCaption;
+    const picPopupCloseButton = addPicPopup.querySelector(
+      "div.picPopup__container .picPopup__close-button"
+    );
+    picPopupCloseButton.addEventListener("click", function () {
+      picPopupCloseButton.closest(".picPopup").remove();
+    });
+    document.body.prepend(addPicPopup);
+  }
+  const picPopupButton = cardArr[i].querySelector(".card__pic-button");
+  picPopupButton.addEventListener("click", picPopup);
+}
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------
